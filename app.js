4b3cd89c275a130b23ea72f008cf8a0b471dca70
mwbramble <mwbramble@gmail.com>
2025-05-08 21:40:36 -0400

    changes to PB section

---
+++ app.js
@@ -0,0 +1,57 @@
+const d = document;
+const baseURL = 'https://www.speedrun.com/api/v1/';
+
+fetch(baseURL + `users/v8l66r8m/personal-bests?embed=game,category`)
+  .then((res) => {
+    res.json()
+    .then((r) => findRuns(r))
+  });
+
+function findRuns(runs){
+  console.log(runs)
+  for(let i = 0; i < runs.data.length; i++){
+    let time = runs.data[i].run.times.primary_t;
+    let h = Math.floor(time / 3600);
+    let m = Math.floor((time % 3600 / 60));
+    let s = (time % 60).toFixed(0);
+
+    if(m < 10 || m < 10 && h > 0){
+      m = '0' + m.toString()
+    }
+
+    if(s < 10){
+      s = '0' + s.toString()
+    }
+
+    let timeStr = h.toString() + ':' + m.toString() + ':' + s.toString();
+
+    let dateFix = new Date(runs.data[i].run.date).toLocaleDateString('en-US', {year: 'numeric', month: 'short', day: 'numeric'});
+
+    if(runs.data[i].game.data.names.international === 'Super Mario World'){
+      d.getElementById('smw-table').innerHTML +=
+        (`
+          <tr>
+            <td>${runs.data[i].category.data.name}</td>
+            <td>${dateFix}</td>
+            <td>${timeStr}</td>
+            <!-- <td>${runs.data[i].place}</td> --!>
+          </tr>
+        `)
+    }
+    if(runs.data[i].game.data.names.international === 'Super Mario Sunshine'){
+      if(runs.data[i].category.data.name !== 'Individual World'){
+        if(runs.data[i].category.data.name !== 'Full Completion'){
+        d.getElementById('sms-table').innerHTML +=
+          (`
+            <tr>
+              <td>${runs.data[i].category.data.name}</td>
+              <td>${dateFix}</td>
+              <td>${timeStr}</td>
+              <!-- <td>${runs.data[i].place}</td> --!>
+            </tr>
+          `)
+        }
+      }
+    }
+  }
+}
